a339d36fd734dbe5450f50633ecff34436aafcc2
elevatebart <ledouxb@gmail.com>
2022-07-27 10:02:56 -0500

    add styleguidist to the repro

---
+++ styleguide.config.js
@@ -0,0 +1,39 @@
+const { getWebpackConfig } = require('@nuxt/bridge')
+
+const FILTERED_PLUGINS = [
+    'WebpackBarPlugin',
+    'VueSSRClientPlugin',
+    'HotModuleReplacementPlugin',
+    'FriendlyErrorsWebpackPlugin',
+    'HtmlWebpackPlugin',
+]
+
+/** @type import("vue-styleguidist").Config */
+module.exports = async () => {
+    // get the webpack config directly from nuxt
+    const nuxtWebpackConfig = await getWebpackConfig('client', {
+        for: 'dev',
+    })
+
+    const webpackConfig = {
+        module: {
+            rules: [
+                ...nuxtWebpackConfig.module.rules.filter(
+                    // remove the eslint-loader
+                    (a) => a.loader !== 'eslint-loader',
+                ),
+            ],
+        },
+        resolve: { ...nuxtWebpackConfig.resolve },
+        plugins: [
+            ...nuxtWebpackConfig.plugins.filter(
+                // And some other plugins that could conflcit with ours
+                (p) => !FILTERED_PLUGINS.includes(p.constructor.name),
+            ),
+        ],
+    }
+
+    return {
+        webpackConfig,
+    }
+}
